bf87db56e432158ce51eaa16cabcc1b8e607b56b
thiagomout <thiagomoutinho19@gmail.com>
2022-10-14 10:17:40 -0300

    security changes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { Client } from "discord.js";
+
+config();
+
+const client = new Client({ intents: ['Guilds' , 'GuildMessages'] });
+
+
+const TOKEN = process.env.BOT_TOKEN;
+
+client.login(TOKEN);
+
+
+
+
